a4afd42b8158e0a2ec1fd583077057a180ebd738
Chirag <jchirag57@gmail.com>
2024-09-09 23:06:26 +0530

    LinkedList September 9

--- JavaLearnings/src/LinkedLists/LinkedList.java
+++ JavaLearnings/src/LinkedLists/LinkedList.java
@@ -1,5 +1,103 @@
 package LinkedLists;
 
-public class LinkedList1 {
-    
-}
+
+import com.sun.jdi.connect.spi.TransportService;
+
+import javax.swing.*;
+import java.security.PublicKey;
+
+public class LinkedList {
+
+    private Node head;
+    private  Node tail;
+    private int length;
+
+     class Node{  // Nested class
+
+        int value ;
+        Node next;
+
+         Node(int value) {
+             this.value = value;
+         }
+    }
+    public LinkedList(int value)
+    {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length =1;
+
+    }
+
+    public  void printList()
+    {
+        Node temp = head;
+
+        while(temp!= null)
+        {
+            System.out.println(temp.value);
+            temp=temp.next;
+
+        }
+    }
+    public void getHead()
+    {
+        System.out.println("The Head value is " + head.value);
+    }
+    public void getTail()
+    {
+        System.out.println("The Tail value is "+ tail.value);
+    }
+
+    public void Append(int value)
+    {
+       Node newNode = new Node(value);
+       if(length==0)
+       {
+           head = newNode;
+           tail = newNode;
+       }
+       else {
+           tail.next=newNode;
+           tail=newNode;
+       }
+       length++;
+    }
+
+
+    public Node removeLast()
+    {
+        if(length==0) return null;
+
+        Node temp = head;
+        Node Pre = head;
+
+        while(temp.next!=null)
+        {
+            Pre=temp;
+            temp =temp.next;
+
+        }
+        tail= Pre;
+        tail.next=null;
+        length--;
+        if(length==0) head=null; tail=null;
+
+        return temp;
+    }
+    public  void prePend(int value)
+    {
+        Node newNode = new Node(value);
+
+        if(length==0)
+        {head=newNode; tail = newNode;}
+        else{
+            newNode.next= head;
+            head= newNode;
+           }
+        length++;
+    }
+    }
+
+
--- JavaLearnings/src/LinkedLists/Main.java
+++ JavaLearnings/src/LinkedLists/Main.java
@@ -1,4 +1,26 @@
 package LinkedLists;
 
-public class Pointers {
+public class Main {
+    public static void main(String[] args) {
+  LinkedList myLinkedList = new LinkedList(4);
+myLinkedList.Append(5);
+myLinkedList.Append(9);
+myLinkedList.Append(8);
+//myLinkedList.getHead();
+//  myLinkedList.getTail();
+
+
+  myLinkedList.printList();
+
+        System.out.println("The removed value is "+ myLinkedList.removeLast().value);
+        System.out.println("The removed value is "+ myLinkedList.removeLast().value);
+        System.out.println("The removed value is "+ myLinkedList.removeLast().value);
+        System.out.println("The removed value is "+ myLinkedList.removeLast().value);
+        System.out.println(myLinkedList.removeLast());
+
+
+
+
+
+    }
 }
